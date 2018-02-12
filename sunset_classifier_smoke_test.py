79d2c6cdfa43deceae983e075dba9547e86235f2
Adam Derewecki <derewecki@gmail.com>
2018-02-11 21:49:51 -0800

    g add sunset_classifier_smoke_test.py

---
+++ sunset_classifier_smoke_test.py
@@ -0,0 +1,5 @@
+from sunset_classifier import SunsetClassifier
+
+sc = SunsetClassifier()
+
+print(sc.classify('https://scontent-sjc3-1.cdninstagram.com/vp/57b14e4ee8e0951ea260b6cbde3b4519/5B1B2134/t51.2885-15/e35/27892138_2450634111828815_5163145710266220544_n.jpg'))
